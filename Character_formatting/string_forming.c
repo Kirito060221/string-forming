48be560325c148a349ccce6e0df4089c9d9350bd
mitsuki870 <mitsuki8177@gmail.com>
2026-06-09 15:01:27 +0900

    ちょい追加

--- Character_formatting/string_forming.c
+++ Character_formatting/string_forming.c
@@ -3,10 +3,14 @@
 int main()
 {
     FILE* fp;
+    char str[1000];
     int n, i;
 
     // 01234...と表示
     for (i = 1; i < n; i++) {
         printf("%d", i % 10);
+    }
+    printf("\n\n");
+    while (fscanf(fp, "%s", str) != EOF) {
     }
 }
