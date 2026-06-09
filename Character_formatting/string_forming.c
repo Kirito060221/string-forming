a62424fc61737ec465fc8b6ad1335cc8dcd1b2d9
mitsuki870 <mitsuki8177@gmail.com>
2026-06-09 23:17:07 +0900

    覚醒

--- Character_formatting/string_forming.c
+++ Character_formatting/string_forming.c
@@ -7,48 +7,48 @@ int handle_special_command(char word[], int* current_len, FILE* fp);
 int main()
 {
     FILE* fp;
-    char word[BUFSIZE];
-    int n, i = 0, current_len;
+    int line_len, i = 0, current_len;
     char fname[BUFSIZE];
     char chr;
+    char line_buf[BUFSIZE];
     printf("整形するファイル名を入力してください：");
     scanf("%s", fname);
-    printf("１行あたりの文字数を入力してください");
-    scanf("%d", n);
+    printf("１行あたりの文字数を入力してください：");
+    scanf("%d", &line_len);
 
     fp = fopen(fname, "r");
     if (fp == NULL) {
         printf("%s file not open!\n", fname);
         return -1;
     }
-    /*
-        while((chr = fgrtc(fp)) != EOF) {
-            putchar(chr);
-        }
-    */
 
     // 01234...と表示
-    for (i = 1; i < n; i++) {
+    for (i = 1; i <= line_len; i++) {
         printf("%d", i % 10);
     }
     printf("\n\n");
-    while (fscanf(fp, "%s", word) != EOF) {
-        if (word[0] == '/' && word[strlen(word) - 1] == '/') {
-            handle_special_command(word, &current_len, fp);
-        } else {
-            // 入れていい文字と入れる文字数の比較
-            if (strlen(word) + current_len > n) {
-                printf("\n");
-                current_len = 0;
+    while (fgets(line_buf, BUFSIZE, fp) != NULL) {
+        char* word = strtok(line_buf, " \n");
+        while (word != NULL) {
+            if (strlen(word) > line_len) {
+                printf("\n文中の単語%sが%dを超えています", word, line_len);
+                return -1;
+            } else if (word[0] == '/' && word[strlen(word) - 1] == '/') {
+                handle_special_command(word, &current_len, fp);
+            } else {
+                if (strlen(word) + current_len > line_len) {
+                    printf("\n");
+                    current_len = 0;
+                }
+                printf("%s ", word);
+                current_len = current_len + strlen(word) + 1;
             }
-            printf("%s ", word);
-            current_len = current_len + strlen(word) + 1;
-            // printf("(%d)", n);
+            word = strtok(NULL, " \n");
         }
     }
     printf("\n\n");
     fclose(fp);
-    for (i = 1; i < n; i++) {
+    for (i = 1; i <= line_len; i++) {
         printf("%d", i % 10);
     }
 
