d6f806b777e6196d1133fd248653aea53d6c0bf7
(     ) <hn24065@student.miyazaki-u.ac.jp>
2026-06-09 14:58:57 +0900

    main文の特殊コマンド追加

--- Character_formatting/string_forming.c
+++ Character_formatting/string_forming.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+
+int handle_special_command(char word[], int *current_len, FILE *fp);
+
 int main()
 {
+}
+
+int handle_special_command(char word[], int *current_len, FILE *fp)
+{
+  if (strcmp(word, "/par/") == 0)
+  {
+    printf("\n\n      ");
+    *current_len = 6;
+    return 1;
+  }
+  else if (strcmp(word, "/break/") == 0)
+  {
+    printf("\n");
+    *current_len = 0;
+    return 1;
+  }
+  else if (strcmp(word, "/space/") == 0)
+  {
+    printf("\n\n");
+    *current_len = 0;
+    return 1;
+  }
+  return 0;
 }
