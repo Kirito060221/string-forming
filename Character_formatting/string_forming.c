8f50f19fc6387bdc0df765f8fe403fbaa2194a1a
tagata_akito <hn24171@student.miyazaki-u.ac.jp>
2026-06-09 15:01:14 +0900

    file入力の記述

--- Character_formatting/string_forming.c
+++ Character_formatting/string_forming.c
@@ -3,4 +3,21 @@
 int main()
 {
     FILE* fp;
+    char fname[] = "input.txt";
+    char chr;
+    int i=0;
+
+    fp = fopen(fname, "r");
+    if(fp == NULL) {
+        printf("%s file not open!\n", fname);
+        return -1;
+    }
+
+    while((chr = fgrtc(fp)) != EOF) {
+        putchar(chr);
+    }
+
+    fclose(fp);
+
+    return 0;
 }
